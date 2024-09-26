1d95a4f69f1798d4de01faf4612c4ec12a433177
Patrick D'appollonio <930925+patrickdappollonio@users.noreply.github.com>
2024-09-26 19:11:54 -0400

    Document functions.

--- internal/civo/sdk/structs.go
+++ internal/civo/sdk/structs.go
@@ -5,7 +5,7 @@ import (
 	"errors"
 )
 
-// Civoer is the interface that wraps the Do method.
+// Civoer is the interface that represents a high-level Civo client.
 type Civoer interface {
 	Do(ctx context.Context, location, method string, output interface{}, params map[string]string) error
 	GetRegion() string
@@ -31,6 +31,8 @@ type APIResource interface {
 	GetResourceType() string
 }
 
+// Compile-time assertions for each type implementing the APIResource interface.
+// This ensures that the types are correctly implemented.
 var (
 	_ APIResource = &Instance{}
 	_ APIResource = &Firewall{}
@@ -54,11 +56,12 @@ type Instance struct {
 	Status     string `json:"status"`
 }
 
-func (i Instance) GetID() string           { return i.ID }
-func (i Instance) GetName() string         { return i.Name }
-func (i Instance) GetAPIEndpoint() string  { return "/v2/instances" }
-func (i Instance) IsSinglePaged() bool     { return false }
-func (i Instance) GetResourceType() string { return "instance" }
+func (i Instance) GetID() string               { return i.ID }            // GetID returns the ID of the instance.
+func (i Instance) GetName() string             { return i.Name }          // GetName returns the name of the instance.
+func (i Instance) GetAPIEndpoint() string      { return "/v2/instances" } // GetAPIEndpoint returns the API endpoint for instances.
+func (i Instance) IsSinglePaged() bool         { return false }           // IsSinglePaged returns whether the resource is single paged.
+func (i Instance) GetResourceType() string     { return "instance" }      // GetResourceType returns the type of the resource.
+func (i Instance) ConsumeOtherResources() bool { return false }           // ConsumeOtherResources returns whether the resource blocks deletion of others.
 
 // Firewall is a Civo firewall.
 type Firewall struct {
@@ -70,11 +73,11 @@ type Firewall struct {
 	NetworkID         string `json:"network_id"`
 }
 
-func (f Firewall) GetID() string           { return f.ID }
-func (f Firewall) GetName() string         { return f.Name }
-func (f Firewall) GetAPIEndpoint() string  { return "/v2/firewalls" }
-func (f Firewall) IsSinglePaged() bool     { return true }
-func (f Firewall) GetResourceType() string { return "firewall" }
+func (f Firewall) GetID() string           { return f.ID }            // GetID returns the ID of the firewall.
+func (f Firewall) GetName() string         { return f.Name }          // GetName returns the name of the firewall.
+func (f Firewall) GetAPIEndpoint() string  { return "/v2/firewalls" } // GetAPIEndpoint returns the API endpoint for firewalls.
+func (f Firewall) IsSinglePaged() bool     { return true }            // IsSinglePaged returns whether the resource is single paged.
+func (f Firewall) GetResourceType() string { return "firewall" }      // GetResourceType returns the type of the resource.
 
 // Volume is a Civo volume.
 type Volume struct {
@@ -86,11 +89,11 @@ type Volume struct {
 	Status     string `json:"status"`
 }
 
-func (v Volume) GetID() string           { return v.ID }
-func (v Volume) GetName() string         { return v.Name }
-func (v Volume) GetAPIEndpoint() string  { return "/v2/volumes" }
-func (v Volume) IsSinglePaged() bool     { return true }
-func (v Volume) GetResourceType() string { return "volume" }
+func (v Volume) GetID() string           { return v.ID }          // GetID returns the ID of the volume.
+func (v Volume) GetName() string         { return v.Name }        // GetName returns the name of the volume.
+func (v Volume) GetAPIEndpoint() string  { return "/v2/volumes" } // GetAPIEndpoint returns the API endpoint for volumes.
+func (v Volume) IsSinglePaged() bool     { return true }          // IsSinglePaged returns whether the resource is single paged.
+func (v Volume) GetResourceType() string { return "volume" }      // GetResourceType returns the type of the resource.
 
 // KubernetesCluster is a Civo Kubernetes cluster.
 type KubernetesCluster struct {
@@ -104,11 +107,11 @@ type KubernetesCluster struct {
 	Instances  []Instance `json:"instances"`
 }
 
-func (k KubernetesCluster) GetID() string           { return k.ID }
-func (k KubernetesCluster) GetName() string         { return k.Name }
-func (k KubernetesCluster) GetAPIEndpoint() string  { return "/v2/kubernetes/clusters" }
-func (k KubernetesCluster) IsSinglePaged() bool     { return false }
-func (k KubernetesCluster) GetResourceType() string { return "kubernetes cluster" }
+func (k KubernetesCluster) GetID() string           { return k.ID }                      // GetID returns the ID of the Kubernetes cluster.
+func (k KubernetesCluster) GetName() string         { return k.Name }                    // GetName returns the name of the Kubernetes cluster.
+func (k KubernetesCluster) GetAPIEndpoint() string  { return "/v2/kubernetes/clusters" } // GetAPIEndpoint returns the API endpoint for Kubernetes clusters.
+func (k KubernetesCluster) IsSinglePaged() bool     { return false }                     // IsSinglePaged returns whether the resource is single paged.
+func (k KubernetesCluster) GetResourceType() string { return "kubernetes cluster" }      // GetResourceType returns the type of the resource.
 
 // Network is a Civo network.
 type Network struct {
@@ -117,11 +120,11 @@ type Network struct {
 	Status string `json:"status"`
 }
 
-func (n Network) GetID() string           { return n.ID }
-func (n Network) GetName() string         { return n.Name }
-func (n Network) GetAPIEndpoint() string  { return "/v2/networks" }
-func (n Network) IsSinglePaged() bool     { return true }
-func (n Network) GetResourceType() string { return "network" }
+func (n Network) GetID() string           { return n.ID }           // GetID returns the ID of the network.
+func (n Network) GetName() string         { return n.Name }         // GetName returns the name of the network.
+func (n Network) GetAPIEndpoint() string  { return "/v2/networks" } // GetAPIEndpoint returns the API endpoint for networks.
+func (n Network) IsSinglePaged() bool     { return true }           // IsSinglePaged returns whether the resource is single paged.
+func (n Network) GetResourceType() string { return "network" }      // GetResourceType returns the type of the resource.
 
 // ObjectStore is a Civo object store.
 type ObjectStore struct {
@@ -134,11 +137,11 @@ type ObjectStore struct {
 	Status string `json:"status"`
 }
 
-func (o ObjectStore) GetID() string           { return o.ID }
-func (o ObjectStore) GetName() string         { return o.Name }
-func (o ObjectStore) GetAPIEndpoint() string  { return "/v2/objectstores" }
-func (o ObjectStore) IsSinglePaged() bool     { return false }
-func (o ObjectStore) GetResourceType() string { return "object store" }
+func (o ObjectStore) GetID() string           { return o.ID }               // GetID returns the ID of the object store.
+func (o ObjectStore) GetName() string         { return o.Name }             // GetName returns the name of the object store.
+func (o ObjectStore) GetAPIEndpoint() string  { return "/v2/objectstores" } // GetAPIEndpoint returns the API endpoint for object stores.
+func (o ObjectStore) IsSinglePaged() bool     { return false }              // IsSinglePaged returns whether the resource is single paged.
+func (o ObjectStore) GetResourceType() string { return "object store" }     // GetResourceType returns the type of the resource.
 
 // ObjectStoreCredential is a Civo object store credential.
 type ObjectStoreCredential struct {
@@ -147,11 +150,11 @@ type ObjectStoreCredential struct {
 	Status string `json:"status"`
 }
 
-func (o ObjectStoreCredential) GetID() string           { return o.ID }
-func (o ObjectStoreCredential) GetName() string         { return o.Name }
-func (o ObjectStoreCredential) GetAPIEndpoint() string  { return "/v2/objectstore/credentials" }
-func (o ObjectStoreCredential) IsSinglePaged() bool     { return false }
-func (o ObjectStoreCredential) GetResourceType() string { return "object store credential" }
+func (o ObjectStoreCredential) GetID() string           { return o.ID }                          // GetID returns the ID of the object store credential.
+func (o ObjectStoreCredential) GetName() string         { return o.Name }                        // GetName returns the name of the object store credential.
+func (o ObjectStoreCredential) GetAPIEndpoint() string  { return "/v2/objectstore/credentials" } // GetAPIEndpoint returns the API endpoint for object store credentials.
+func (o ObjectStoreCredential) IsSinglePaged() bool     { return false }                         // IsSinglePaged returns whether the resource is single paged.
+func (o ObjectStoreCredential) GetResourceType() string { return "object store credential" }     // GetResourceType returns the type of the resource.
 
 // SSHKey is a Civo SSH key.
 type SSHKey struct {
@@ -160,11 +163,11 @@ type SSHKey struct {
 	Fingerprint string `json:"fingerprint"`
 }
 
-func (s SSHKey) GetID() string           { return s.ID }
-func (s SSHKey) GetName() string         { return s.Name }
-func (s SSHKey) GetAPIEndpoint() string  { return "/v2/sshkeys" }
-func (s SSHKey) IsSinglePaged() bool     { return true }
-func (s SSHKey) GetResourceType() string { return "ssh key" }
+func (s SSHKey) GetID() string           { return s.ID }          // GetID returns the ID of the SSH key.
+func (s SSHKey) GetName() string         { return s.Name }        // GetName returns the name of the SSH key.
+func (s SSHKey) GetAPIEndpoint() string  { return "/v2/sshkeys" } // GetAPIEndpoint returns the API endpoint for SSH keys.
+func (s SSHKey) IsSinglePaged() bool     { return true }          // IsSinglePaged returns whether the resource is single paged.
+func (s SSHKey) GetResourceType() string { return "ssh key" }     // GetResourceType returns the type of the resource.
 
 // LoadBalancer is a Civo load balancer.
 type LoadBalancer struct {
@@ -174,8 +177,8 @@ type LoadBalancer struct {
 	ClusterID  string `json:"cluster_id"`
 }
 
-func (l LoadBalancer) GetID() string           { return l.ID }
-func (l LoadBalancer) GetName() string         { return l.Name }
-func (l LoadBalancer) GetAPIEndpoint() string  { return "/v2/loadbalancers" }
-func (l LoadBalancer) IsSinglePaged() bool     { return true }
-func (l LoadBalancer) GetResourceType() string { return "load balancer" }
+func (l LoadBalancer) GetID() string           { return l.ID }                // GetID returns the ID of the load balancer.
+func (l LoadBalancer) GetName() string         { return l.Name }              // GetName returns the name of the load balancer.
+func (l LoadBalancer) GetAPIEndpoint() string  { return "/v2/loadbalancers" } // GetAPIEndpoint returns the API endpoint for load balancers.
+func (l LoadBalancer) IsSinglePaged() bool     { return true }                // IsSinglePaged returns whether the resource is single paged.
+func (l LoadBalancer) GetResourceType() string { return "load balancer" }     // GetResourceType returns the type of the resource.
