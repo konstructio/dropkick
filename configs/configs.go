6dd53e6e7df60b72150e3f456a42855de4646a18
Jared Edwards <jared@kubefirst.io>
2024-12-03 16:51:26 -0700

    remove all the extra config from config
    Co-authored-by: Patrick D'appollonio <930925+patrickdappollonio@users.noreply.github.com>

--- configs/configs.go
+++ configs/configs.go
@@ -1,12 +1,3 @@
 package configs
 
-const (
-	DefaultVersion = "development"
-)
-
-//nolint:gochecknoglobals // used to store the version of the built binary
-var Version = DefaultVersion
-
-type Config struct {
-	Version string
-}
+var Version = "development"
