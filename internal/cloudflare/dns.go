181bb259dd0b8157ed45917ccf2a2358b4d10dc1
Jared Edwards <jared@konstruct.io>
2024-12-03 12:07:39 -0700

    debug, not filtering

--- internal/cloudflare/dns.go
+++ internal/cloudflare/dns.go
@@ -18,15 +18,9 @@ func (c *Cloudflare) NukeDNSRecords(ctx context.Context) error {
 		return fmt.Errorf("unable to list records for domain %q: %w", c.zoneName, err)
 	}
 
-	// if subdomain is set filter the records
-	var subdomainRecords []cloudflarego.DNSRecord
+	records = filterRecords(records, c.subdomain)
+
 	if c.subdomain != "" {
-		for _, r := range records {
-			if strings.Contains(r.Name, c.subdomain) {
-				subdomainRecords = append(subdomainRecords, r)
-			}
-		}
-		records = subdomainRecords
 		c.logger.Infof("found %d records for %q", len(records), fmt.Sprintf("%s.%s", c.subdomain, c.zoneName))
 	} else {
 		c.logger.Infof("found %d records for %q", len(records), c.zoneName)
@@ -34,14 +28,34 @@ func (c *Cloudflare) NukeDNSRecords(ctx context.Context) error {
 
 	for _, r := range records {
 		if c.nuke {
-			c.logger.Infof("nuke enabled, deleting record %q", r.Name)
+			c.logger.Infof("nuke enabled, deleting record %q - %q", r.Type, r.Name)
 			if err := c.client.DeleteDNSRecord(ctx, &cloudflarego.ResourceContainer{Identifier: c.zoneID}, r.ID); err != nil {
 				return fmt.Errorf("unable to delete record %q: %w", r.Name, err)
 			}
 		} else {
-			c.logger.Warnf("nuke disabled, found record %q", r.Name)
+			c.logger.Warnf("nuke disabled, found record %q - %q", r.Type, r.Name)
 		}
 	}
 
 	return nil
+}
+
+func filterRecords(records []cloudflarego.DNSRecord, subdomain string) []cloudflarego.DNSRecord {
+	var filteredRecords []cloudflarego.DNSRecord
+	aRecord := "A"
+	txtRecord := "TXT"
+	fmt.Println("subdomain: ", subdomain)
+
+	for _, r := range records {
+		if subdomain != "" {
+			if strings.Contains(r.Name, subdomain) && r.Type == txtRecord || r.Type == aRecord {
+				filteredRecords = append(filteredRecords, r)
+			}
+		} else {
+			if r.Type == txtRecord || r.Type == aRecord {
+				filteredRecords = append(filteredRecords, r)
+			}
+		}
+	}
+	return filteredRecords
 }
