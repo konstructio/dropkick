2eba439a09c562fdbeb25c8e2b4fd86203f1df88
Jared Edwards <jared@konstruct.io>
2024-12-03 14:39:51 -0700

    thanks lint... rm newline

--- cmd/version.go
+++ cmd/version.go
@@ -15,7 +15,6 @@ func getVersionCommand() *cobra.Command {
 		Short: "print the version for dropkick cli",
 		Long:  `print the version for dropkick cli`,
 		RunE: func(_ *cobra.Command, _ []string) error {
-
 			log := logger.New(io.Writer(os.Stdout))
 
 			log.Infof("dropkick cli version: %q", configs.Version)
